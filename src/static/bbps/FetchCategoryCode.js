06583e2a13763f89fcc293e1b16f3c01971b079f
Anil Kumar <kumarglci@gmail.com>
2025-09-07 13:31:48 +0530

    upi

--- src/static/bbps/FetchCategoryCode.js
+++ src/static/bbps/FetchCategoryCode.js
@@ -1,10 +1,6 @@
 const data = [
     {
-        "Parameter": "tenantId",
-        "DataType": "String",
-        "MaxLength": 20,
-        "Required": "Required",
-        "value":"Merchant Bill Tenant Id"
+       
     }
 ]
 export default data;
